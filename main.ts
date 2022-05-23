7a5b5ef0038df015991e74844684f1291d1fc9a5
dangerousdaven <105022008+dangerousdaven@users.noreply.github.com>
2022-05-23 14:49:14 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+let Motor_2 = 0
+let Motor_1 = 0
+radio.setGroup(1)
+basic.forever(function () {
+    Motor_1 = 0
+    Motor_2 = 0
+    if (input.buttonIsPressed(Button.A) && (input.acceleration(Dimension.X) > -250 && input.acceleration(Dimension.X) < 250)) {
+        Motor_1 = 1
+        Motor_2 = 1
+    } else {
+        if (input.buttonIsPressed(Button.A) && input.acceleration(Dimension.X) >= 250) {
+            Motor_1 = 1
+        } else {
+            Motor_1 = 0
+        }
+        if (input.buttonIsPressed(Button.A) && input.acceleration(Dimension.X) <= -250) {
+            Motor_2 = 1
+        } else {
+            Motor_2 = 0
+        }
+    }
+    radio.sendValue("Motor_1", Motor_1)
+    radio.sendValue("Motor_2", Motor_2)
+})
