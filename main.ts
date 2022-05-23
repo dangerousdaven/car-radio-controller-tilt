98b1677637d4ce9f627fe506e900eccad49f066a
dangerousdaven <105022008+dangerousdaven@users.noreply.github.com>
2022-05-23 16:44:52 +0100

    forwards and backwards

--- main.ts
+++ main.ts
@@ -10,13 +10,20 @@ basic.forever(function () {
     } else {
         if (input.buttonIsPressed(Button.A) && input.acceleration(Dimension.X) >= 250) {
             Motor_1 = 1
-        } else {
-            Motor_1 = 0
         }
         if (input.buttonIsPressed(Button.A) && input.acceleration(Dimension.X) <= -250) {
             Motor_2 = 1
-        } else {
-            Motor_2 = 0
+        }
+    }
+    if (input.buttonIsPressed(Button.B) && (input.acceleration(Dimension.X) > -250 && input.acceleration(Dimension.X) < 250)) {
+        Motor_1 = -1
+        Motor_2 = -1
+    } else {
+        if (input.buttonIsPressed(Button.B) && input.acceleration(Dimension.X) >= 250) {
+            Motor_1 = -1
+        }
+        if (input.buttonIsPressed(Button.B) && input.acceleration(Dimension.X) <= -250) {
+            Motor_2 = -1
         }
     }
     radio.sendValue("Motor_1", Motor_1)
